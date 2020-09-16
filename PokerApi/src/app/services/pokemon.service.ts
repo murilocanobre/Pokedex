f77a72830eeef92845cc4f13919f1453c88c5bc3
Murilo Canobre <mgcmurilo@gmail.com>
2020-09-16 16:52:49 -0300

    finalizado a lista dos pokemons e filtro

--- PokerApi/src/app/services/pokemon.service.ts
+++ PokerApi/src/app/services/pokemon.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  baseUrl: string = environment.baseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //Obtiene pokemon
+  getPokemons(index){
+    return this.http.get<any>(`${this.baseUrl}/pokemon/${index}`);
+  }
+  getEvolutes(index){
+    return this.http.get<any>(`${this.baseUrl}/evolution-chain/${index}`);
+  }
 }
