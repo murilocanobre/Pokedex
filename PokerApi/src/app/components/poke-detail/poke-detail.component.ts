1d6d98be4c213ab7b1552110bac080567c93c10a
Murilo Canobre <mgcmurilo@gmail.com>
2020-09-16 21:32:30 -0300

    detathes de cada pokemon finalizado

--- PokerApi/src/app/components/poke-detail/poke-detail.component.ts
+++ PokerApi/src/app/components/poke-detail/poke-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-poke-detail',
@@ -7,9 +11,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeDetailComponent implements OnInit {
 
-  constructor() { }
+  pokemon: any = '';
+  pokemonImg = '';
+  name = '';
+  type='';
+  hp = [];
+  attack = [];
+  defense = [];
+  speed = [];
+  pokemonEvolute = '';
+  pokemonEvolute1 = '';
+
+
+  constructor(private activatedRouter: ActivatedRoute,
+    private pokemonService: PokemonService, private http: HttpClient) {
+
+    this.activatedRouter.params.subscribe(
+      params => {
+        this.getPokemon(params['id']);
+
+
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
+
+  getPokemon(id) {
+    this.pokemonService.getPokemons(id).subscribe(
+      res => {
+
+        this.pokemon = res;
+        console.log(res)
+        this.name = res.name;
+        this.hp = res.stats[0].base_stat;
+        this.attack = res.stats[1].base_stat;
+        this.defense = res.stats[2].base_stat;
+        this.speed = res.stats[5].base_stat;
+
+
+        this.pokemonImg = this.pokemon.sprites.front_default;
+        for (let i = 1; i <= 427; i++) {
+        this.pokemonService.getEvolutes(i).subscribe(
+
+          res => {
+            if(this.name === res.chain.species.name){
+              this.pokemonEvolute = res.chain.evolves_to[0].species.name;
+              this.pokemonEvolute1 = res.chain.evolves_to[0].evolves_to[0].species.name;
+            }
+
+
+
+
+          },
+          err => {
+            console.log(err);
+          }
+        )}
+
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+
+
 
 }
